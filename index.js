92bd66e097d106f237d3f1db053151c7d6cf298e
Inderpreet02 <inder192002@gmail.com>
2023-07-30 23:13:38 +0530

    fixed cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express();
 const PORT = 8000
+const cors = require("cors")
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/posts", (req, res) => {
     const data = [
